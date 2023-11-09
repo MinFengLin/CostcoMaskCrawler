7267b67f5e5059710eaf8e5b51c926a46a12a13c
MinfengLin <minfenglin0406@gmail.com>
2023-11-09 20:54:26 +0800

    Modified - Dynamic update product list

--- crawler.py
+++ crawler.py
@@ -17,6 +17,22 @@ from bs4 import BeautifulSoup
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
+from typing import List
+
+class Product:
+    def __init__(self, title, url, status=0, price='N/A', id=None):
+        self.title = title
+        self.url = url
+        self.status = status
+        self.price = price
+        self.id = id if id else self.extract_id(url)
+
+    @staticmethod
+    def extract_id(url):
+        # 切割網址取得商品編號
+        if url.rsplit("/", 2)[1] == "p":
+            return url.rsplit("/", 1)[1]
+        return None
 
 class costco:
     def __init__(self):
@@ -63,19 +79,14 @@ class costco:
             self.USER_AGENT_LIST = config["agent"]["user-agent"]
 
         # 設定目標商品
-        self.product = list()
+        self.products: List[Product] = []
         with open(productFileName, "r", encoding="utf-8") as json_file:
-            config = json.load(json_file)
+            product_config = json.load(json_file)
 
-            for item in config:
-                # 檢查網址為商品頁面或分類列表
-                # 切割網址取得商品編號
-                if item["url"].rsplit("/", 2)[1] == "p":
-                    item["id"] = item["url"].rsplit("/", 1)[1]
-                else:
-                    item["id"] = None
-                item["status"] = 0
-                self.product.append(item)
+            for item in product_config:
+                # 創建Product實例並添加到列表
+                self.products.append(Product(item["title"], item["url"]))
+        
         if self.line_bot:
             self.line_bot_api = LineBotApi(self.line_bot_token)
 
@@ -92,12 +103,43 @@ class costco:
         2 :有上架且"可能"有庫存(分類清單中有出現或商品網頁存在，且加入購物車按鈕存在)
         '''
 
+    def get_new_products(self):
+        with open('product.json', 'r', encoding='utf-8') as file:
+            new_products_data = json.load(file)
+            return new_products_data
+
+    def Updateproduct(self):
+        new_products_data = self.get_new_products()   # 讀取產品列表
+
+        # 用來儲存更新後的產品
+        updated_products = []
+
+        # 遍歷新產品數據, 添加新增的產品
+        for new_product_data in new_products_data:
+            if not any(product.url == new_product_data['url'] for product in self.products):
+                new_product = Product(
+                    title=new_product_data['title'],
+                    url=new_product_data['url']
+                )
+                updated_products.append(new_product)
+                logging.info(f"Added new product: {new_product.title}")
+            else:
+                # 如果產品已經存在, 直接添加到列表即可
+                existing_product = next((product for product in self.products if product.url == new_product_data['url']), None)
+                if existing_product:
+                    updated_products.append(existing_product)
+
+        # 更新產品列表
+        self.products = updated_products
+        logging.info("Product list has been updated.")
+
 
     def start(self):
         while True:
             self.nowtime = datetime.now(pytz.timezone("Asia/Taipei"))
             if self.check_time():
                 logging.info("check " + self.nowtime.strftime("%Y-%m-%d %H:%M:%S"))
+                self.Updateproduct()
                 if self.save_check_timestamp_2_file:
                     with open('check_log_record', 'r') as file:
                         lines = file.readlines()
@@ -109,46 +151,55 @@ class costco:
                         with open('check_log_record', 'a') as file:
                             file.write("check " + self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + '\n')
 
-                for item in self.product:
+                no_update = "No Update: "
+                for item in self.products:
                     result = self.search(item)
-                    if result != item["status"]:
-                        item["status"] = result
-                        item["price"] = self.price
-                        logging.info(item["title"] + " " + item["url"])
+                    if result != item.status:
+                        item.status = result
+                        item.price = self.price
+                        logging.info(item.title + " " + item.url)
                         if self.line_notify:
                             self.send_line_notify(item)
                         if self.line_bot:
                             self.send_line_bot(item)
                         if self.email_service:
                             self.send_email(item)
+                    else:
+                        no_update = no_update + item.title + "/"
+                logging.info(no_update)
+
 
             time.sleep(random.randint(10, self.next_search_time))
 
 
     # 爬取資料，檢查按鈕是否存在
-    def search(self, item):
+    def search(self, product):
         header = {
             "user-agent": random.choice(self.USER_AGENT_LIST)
         }
-        with requests.get(item["url"], headers=header) as res:
+        with requests.get(product.url, headers=header) as res:
             soup = BeautifulSoup(res.text, "lxml")
-            span_price_tag = soup.find('span', class_='notranslate ng-star-inserted')
-            self.price = span_price_tag.get_text()
-            if (soup.find('span', class_='you-pay-value') != None):
-                span_save_tag = soup.find('span', class_='you-pay-value')
-                self.price = "原價:" + self.price + ", 特價: " + span_save_tag.get_text()
+            if (soup.find('span', class_='notranslate ng-star-inserted') != None):
+                span_price_tag = soup.find('span', class_='notranslate ng-star-inserted')
+                self.price = span_price_tag.get_text()
 
+                if (soup.find('span', class_='you-pay-value') != None):
+                    span_save_tag = soup.find('span', class_='you-pay-value')
+                    self.price = "原價:" + self.price + ", 特價: " + span_save_tag.get_text()
+            else:
+                self.price = "N/A"
+    
             '''
             商品頁面存在，可以找到 addToCartButton
             商品頁面不存在則會自動跳回分類列表，若分類列表存在商品，可能可以找到 add-to-cart-button-xxxxxx
             出現"加入購物車"按鈕不代表一定有庫存
             若不知道商品網址或編號，只有商品分類網址跟商品名稱，就直接搜尋名稱，但無法檢查按鈕
             '''
-            if item["id"] != None and (soup.find(id="addToCartButton") != None or \
-                                       soup.find(id=("add-to-cart-button-" + item["id"])) != None or \
-                                       soup.find(id="add-to-cart-button") ):
+            if product.id is not None and (soup.find(id="addToCartButton") is not None or \
+                                       soup.find(id=("add-to-cart-button-" + product.id)) is not None or \
+                                       soup.find(id="add-to-cart-button")):
                 return 2
-            elif item["title"] in res.text:
+            elif product.title in res.text:
                 return 1
         return 0
 
@@ -171,12 +222,12 @@ class costco:
 
     # email通知
     def send_email(self, item):
-        text = self.nowtime.strftime("%Y-%m-%d %H:%M:%S ") + item["title"] + "\n" + item["url"]
+        text = self.nowtime.strftime("%Y-%m-%d %H:%M:%S ") + item.price + "\n" + item.title + "\n" + item.url
 
         msg = MIMEText(text, "plain", "utf-8")
         msg["From"] = Header("好市多爬蟲", "utf-8")
         msg["To"] = Header(self.to_addr, "utf-8")
-        msg["Subject"] = Header(self.message[item["status"]], "utf-8")
+        msg["Subject"] = Header(self.message[item.status], "utf-8")
 
         with smtplib.SMTP(self.server, self.port) as smtp:
             smtp.ehlo()
@@ -197,10 +248,10 @@ class costco:
             'Authorization': 'Bearer ' + token    # 設定token
         }
         data = {
-            'message': self.message[item["status"]] + "\n" + \
+            'message': self.message[item.status] + "\n" + \
                        self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + \
-                       item["price"] + "\n" + \
-                       item["title"] + "\n" + item["url"]
+                       item.price + "\n" + \
+                       item.title + "\n" + item.url
         }
         # logging.info(notify_url)
         # logging.info(headers)
@@ -210,8 +261,8 @@ class costco:
 
     # line_bot通知
     def send_line_bot(self, item):
-        text = self.message[item["status"]] + "\n" + \
-               self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + item["title"] + "\n" + item["url"]
+        text = self.message[item.status] + "\n" + \
+               self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + item.price + "\n" + item.title + "\n" + item.url
         try:
             self.line_bot_api.broadcast(TextSendMessage(text=text))
         except LineBotApiError as e:
